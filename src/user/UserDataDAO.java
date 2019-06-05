bc0b2793379318f08ea34366a156f99232f40ede
Juho <Juho@180.64.34.116>
2019-06-05 23:15:22 +0900

    dd

--- src/user/UserDataDAO.java
+++ src/user/UserDataDAO.java
@@ -45,28 +45,9 @@ public class UserDataDAO {
 	}
 	
 	// 1이면 중복, 0이면 중복 아님.
-	public int idDuplicate(String userID) {
-//		String SQL = "SELECT count(*) FROM user_data WHERE userID = ?";
-//		try {
-//			pstmt = conn.prepareStatement(SQL);
-//			pstmt.setString(1, userID);
-//			rs = pstmt.executeQuery();
-//			if (rs.next()) {
-//				if (rs.getInt(1) >= 1)
-//					return 1;  // 중복
-//				else
-//					return 0;  // 중복 아님
-//			}
-//			return 1;  // 중복아님;
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		return -2; // 데이터베이스 오류
-		
+	public int idDuplicate(String userID) {		
 		SqlSession session = MySqlSession.getSession();
-		System.out.println(userID);
 		int result = (Integer)session.selectOne("UserData.duplicate",userID);
-		System.out.println(result);
 		if (result >= 1)
 			return 1;  // 중복
 		else
